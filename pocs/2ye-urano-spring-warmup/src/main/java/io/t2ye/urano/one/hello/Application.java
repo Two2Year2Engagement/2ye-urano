abc06a6e8ece964ba88ff7f6e6658af0b5398ca3
ragnarokkrr <ragnarokkrr@gmail.com>
2015-07-29 22:39:02 -0300

    first sample - clean with gitignore

---
+++ pocs/2ye-urano-spring-warmup/src/main/java/io/t2ye/urano/one/hello/Application.java
@@ -0,0 +1,15 @@
+package io.t2ye.urano.one.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ragnarokkrr on 7/29/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
