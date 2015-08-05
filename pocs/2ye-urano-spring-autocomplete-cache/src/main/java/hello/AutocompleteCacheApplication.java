13dd3dcffce8ec7ad77605dd8c7c0a2ae01f10a6
willmenn <willahrons@hotmail.com>
2015-08-05 01:06:08 -0300

    comitando EnableCaching

--- pocs/2ye-urano-spring-autocomplete-cache/src/main/java/hello/AutocompleteCacheApplication.java
+++ pocs/2ye-urano-spring-autocomplete-cache/src/main/java/hello/AutocompleteCacheApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class AutocompleteCacheApplication implements CommandLineRunner {
 	@Autowired
 	private PersonRepository repository;
